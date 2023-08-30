e52109aed535371b999a4619680e722b87c788a6
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-30 19:24:48 +0000

    useState functional hiughlighting the clicked traffic light.

--- src/js/component/traffic_light.jsx
+++ src/js/component/traffic_light.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/index.css"
 
 //create your first component
 const TrafficLight = () => {
+
+	const [ highlight, setHighlight ] = useState("");
+
 	return (
 		<div className="container-fluid d-flex vh-100 align-items-center justify-content-center">
 			
 			<div className="traffic-pole container d-flex align-items-center bg-dark">
 				<div className="traffic-lights">
-					<div className="light red bg-danger my-3">
+					<div className={`light red bg-danger my-3 ${highlight === "red" ? "selected" : ""}`} onClick={() => setHighlight("red")}>
 						&nbsp;
 					</div>
 
-					<div className="light yellow bg-warning my-3">
+					<div className={`light yellow bg-warning my-3 ${highlight === "yellow" ? "selected" : ""}`} onClick={() => setHighlight("yellow")}>
 						&nbsp;
 					</div>
 
-					<div className="light green bg-success my-3">
+					<div className={`light green bg-success my-3 ${highlight === "green" ? "selected" : ""}`} onClick={() => setHighlight("green")}>
 						&nbsp;
 					</div>
 				</div>
